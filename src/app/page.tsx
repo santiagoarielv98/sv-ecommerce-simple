902615d4fb0d962ca05732ab9f849fc26f73ec68
Santiago Ariel <santiagoarielv98@gmail.com>
2025-02-01 14:32:07 -0300

    Refactor Home component to use Prisma for fetching products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import ProductList from "@/components/product-list";
-import client from "@/lib/client";
+import { prisma } from "@/lib/prisma";
 import { Container, Grid2 as Grid, Toolbar } from "@mui/material";
 
 export default async function Home() {
-  const db = (await client).db();
-  const products = await db.collection("products").find({}).toArray();
+  const products = await prisma.product.findMany();
 
   return (
     <Container>

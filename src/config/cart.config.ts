475878145b52f6bccb3fbd5c17c1aa44acb594b6
Santiago Ariel <santiagoarielv98@gmail.com>
2025-02-04 19:10:49 -0300

    Refactor: remove unused components and types, add HomePage component

--- src/config/cart.config.ts
+++
@@ -1,2 +0,0 @@
-export const CART_STORAGE_KEY = "shopping-cart";
-export const MAX_QUANTITY_PER_ITEM = 5;
--- src/types/pagination.d.ts
+++
@@ -1,4 +0,0 @@
-import type { GridPaginationModel, GridSortItem } from "@mui/x-data-grid";
-
-export type PaginationOptions = GridPaginationModel &
-  GridSortItem & { searchQuery?: string };

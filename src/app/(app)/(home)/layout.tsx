b6f2361c83cb99f3fcc1ee8231ed832a166cb68b
Santiago Ariel <santiagoarielv98@gmail.com>
2025-02-05 00:26:55 -0300

    Refactor: add layout and page components for Home, Cart, Checkout, Orders, and Product sections

---
+++ src/app/(app)/(home)/layout.tsx
@@ -0,0 +1,8 @@
+import Box from "@mui/material/Box";
+import React from "react";
+
+const HomeLayout = ({ children }: React.PropsWithChildren) => {
+  return <Box>{children}</Box>;
+};
+
+export default HomeLayout;
---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,8 @@
+import Box from "@mui/material/Box";
+import React from "react";
+
+const AuthLayout = ({ children }: React.PropsWithChildren) => {
+  return <Box>{children}</Box>;
+};
+
+export default AuthLayout;

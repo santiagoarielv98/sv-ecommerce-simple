bc09c4d5c6ac5a9f1095580385b77e85b66276b7
Santiago Ariel <santiagoarielv98@gmail.com>
2025-02-05 20:13:17 -0300

    Refactor: implement Orders page with OrderCard component and loading skeleton for improved user experience

---
+++ src/app/(app)/orders/_components/order-card.tsx
@@ -0,0 +1,48 @@
+import Chip from "@mui/material/Chip";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import type { Order, OrderItem, ShippingAddress } from "@prisma/client";
+
+export interface OrderCardProps {
+  order: Order & {
+    items: OrderItem[];
+  } & {
+    shippingAddress: ShippingAddress;
+  };
+}
+
+const OrderCard = ({ order }: OrderCardProps) => {
+  return (
+    <Paper
+      sx={{
+        p: 3,
+      }}
+    >
+      <Stack direction="row" justifyContent="space-between" mb={1}>
+        <Typography variant="h6">Order #{order.id.slice(-6)}</Typography>
+        <Chip
+          label={order.status}
+          color={order.status === "PENDING" ? "warning" : "success"}
+        />
+      </Stack>
+      <Stack direction="row" justifyContent="space-between" mb={2}>
+        <Typography variant="body2" color="text.secondary">
+          {new Date(order.createdAt).toLocaleDateString()}
+        </Typography>
+        <Typography variant="h6">${order.total.toFixed(2)}</Typography>
+      </Stack>
+      <Stack>
+        <Typography variant="body2" color="text.secondary">
+          {order.items.length} items
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Shipping to: {order.shippingAddress?.city},{" "}
+          {order.shippingAddress?.country}
+        </Typography>
+      </Stack>
+    </Paper>
+  );
+};
+
+export default OrderCard;
---
+++ src/app/(app)/orders/page.tsx
@@ -0,0 +1,85 @@
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid2";
+import MuiLink from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import type { OrderItem, ShippingAddress } from "@prisma/client";
+import Link from "next/link";
+import OrderCard from "./_components/order-card";
+
+const orders = [
+  {
+    id: "1",
+    userId: "1",
+    status: "PENDING",
+    total: 123.45,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    paymentId: "",
+    paymentUrl: "",
+    items: [
+      {
+        id: "1",
+        price: 12.34,
+        quantity: 2,
+      },
+      {
+        id: "2",
+        name: "Product 2",
+        price: 34.56,
+        quantity: 1,
+      },
+    ] as OrderItem[],
+    shippingAddress: {
+      city: "San Francisco",
+      country: "United States",
+    } as ShippingAddress,
+  },
+  {
+    id: "2",
+    userId: "1",
+    status: "SHIPPED",
+    total: 234.56,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    paymentId: "",
+    paymentUrl: "",
+    items: [
+      {
+        id: "3",
+        price: 45.67,
+        quantity: 3,
+      },
+    ] as OrderItem[],
+    shippingAddress: {
+      city: "Los Angeles",
+      country: "United States",
+    } as ShippingAddress,
+  },
+] as const;
+
+export default async function OrdersPage() {
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  return (
+    <Container sx={{ my: 2 }}>
+      <Stack spacing={2}>
+        <Typography variant="h4" gutterBottom>
+          My Orders
+        </Typography>
+        <Grid container spacing={3}>
+          {orders.map((order) => (
+            <Grid key={order.id} size={{ xs: 12 }}>
+              <MuiLink
+                href={`/orders/${order.id}`}
+                component={Link}
+                underline="none"
+              >
+                <OrderCard order={order} />
+              </MuiLink>
+            </Grid>
+          ))}
+        </Grid>
+      </Stack>
+    </Container>
+  );
+}

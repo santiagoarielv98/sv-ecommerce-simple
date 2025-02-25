0749b96b2d563307f81525eed5fba2da5224156c
Santiago Ariel <santiagoarielv98@gmail.com>
2025-02-25 13:36:51 -0300

    Refactor Logo component to use Typography for improved accessibility and styling; streamline layout and remove unnecessary Box wrapper

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,35 +1,39 @@
-import React from "react";
-
+import { ROUTE } from "@/config/route";
 import ShoppingCartCheckoutSharpIcon from "@mui/icons-material/ShoppingCartCheckoutSharp";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
-import Box from "@mui/material/Box";
-import { ROUTE } from "@/config/route";
 
 const Logo = () => {
   return (
-    <Box sx={{ display: "flex", alignItems: "center" }}>
-      <ShoppingCartCheckoutSharpIcon sx={{ mr: 1 }} />
+    <Typography
+      component={Link}
+      href={ROUTE.HOME}
+      variant="h6"
+      noWrap
+      sx={{
+        display: "inline-flex",
+        alignItems: "center",
+        color: "inherit",
+        textDecoration: "none",
+        fontWeight: "bold",
+        fontSize: "0.75rem",
+        gap: 1,
+      }}
+    >
+      <ShoppingCartCheckoutSharpIcon />
       <Typography
-        component={Link}
-        href={ROUTE.HOME}
-        variant="h6"
-        noWrap
+        variant="inherit"
         sx={{
+          mr: 2,
           display: {
             xs: "none",
             md: "block",
           },
-          mr: 2,
-          color: "inherit",
-          textDecoration: "none",
-          fontWeight: "bold",
-          fontSize: "0.75rem",
         }}
       >
         SV ECOMMERCE
       </Typography>
-    </Box>
+    </Typography>
   );
 };
 
